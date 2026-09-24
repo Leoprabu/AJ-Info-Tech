ff3239e8d804836864eec2115d56df26c84aa621
Leoprabu <48479657+Leoprabu@users.noreply.github.com>
2026-09-24 18:01:35 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
-  host: 'http://sql309.infinityfree.com:3306/',
+  host: 'sql309.infinityfree.com',
   user: 'if0_42221348',
   password: 'Leoprabu037',            // ⚠️ set your MySQL password
   database: 'if0_42221348_ajinfotech',
+  port: '3306'
   waitForConnections: true,
   connectionLimit: 10,
   dateStrings: true
